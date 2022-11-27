982f539da15c6b3bbe5c2efdcc605f25ce7e53ae
Anita-Mahmud <anita.mahmud13@gmail.com>
2022-11-27 16:45:29 +0600

    db connected

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 
 //middleware
 app.use(express.json());
 app.use(cors());
+
+//mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qspwkqa.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+console.log(uri);
 
 app.get('/',async(req,res)=>{
     res.send('Share Knowledge Server is running' );
