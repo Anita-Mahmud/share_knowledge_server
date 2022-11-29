4afbc0dcc81d9941aa893d0141c52f02a4332c42
Anita-Mahmud <anita.mahmud13@gmail.com>
2022-11-29 12:56:17 +0600

    categories added

--- index.js
+++ index.js
@@ -10,11 +10,27 @@ app.use(express.json());
 app.use(cors());
 
 //mongodb
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qspwkqa.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-console.log(uri);
+//db
+async function run() {
+    try {
+      const db = client.db("shareKnowledge");
+      const categoriesCollection = db.collection("categories");
+
+      //categoris
+      app.get('/categories',async(req,res)=>{
+        const query = {};
+        const categories = await categoriesCollection.find(query).toArray();
+        res.send(categories)
+      })
+    
+    } finally {
+      
+    }
+  }
+  run().catch(console.dir);
 
 app.get('/',async(req,res)=>{
     res.send('Share Knowledge Server is running' );
