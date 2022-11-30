1df5da5e3c9c75ee50a2dad5906bed8fdd88d002
Anita-Mahmud <anita.mahmud13@gmail.com>
2022-11-30 17:05:21 +0600

    admin and seller route

--- index.js
+++ index.js
@@ -40,6 +40,20 @@ async function run() {
       const users = await usersCollection.find(query).toArray();
       res.send(users)
     });
+    //admin
+    app.get('/users/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email }
+      const user = await usersCollection.findOne(query);
+      res.send({ isAdmin: user?.role === 'admin' });
+  })
+  //seller
+  app.get('/users/seller/:email', async (req, res) => {
+    const email = req.params.email;
+    const query = { email }
+    const user = await usersCollection.findOne(query);
+    res.send({ isSeller: user?.role === 'Seller' });
+})
      app.post('/users', async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
